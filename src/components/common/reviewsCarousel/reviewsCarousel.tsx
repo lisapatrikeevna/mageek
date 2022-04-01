ba6d2e4302774ac350b4e72233ef19f8342d6f61
Natalia Bondarenko <lisa15.08patrikeevna@gmail.com>
2022-04-02 02:08:04 +0300

    ReviewsCarousel

--- src/components/common/reviewsCarousel/reviewsCarousel.tsx
+++ src/components/common/reviewsCarousel/reviewsCarousel.tsx
@@ -2,28 +2,41 @@ import React from 'react';
 import {Carousel} from "react-responsive-carousel";
 import {Link} from "react-router-dom";
 import './carouselReviev.css'
+import StarIcon from '@mui/icons-material/Star';
 
 
 type itemBannerType = {
     img: any
-    // img:object
+    rating:number
     desk: string
-    title: string
-    btnText: string
-    btnLink: string
+    userName: string
+    text: string
+    btnText?: string
+    btnLink?: string
 }
 type propsType = {
     banner: Array<itemBannerType>
 }
 
 const ReviewsCarousel = (props: propsType) => {
-    const itemBanner = props.banner.map(i => <div className="itemWork">
-        <img src={i.img} alt='img'/>
-        <div className="legendReview">
-            i i i i i
-            <h3 className='titleOffer'>{i.title}</h3>
+    const itemBanner = props.banner.map(i => <div className="itemReview">
+        <div className="legendReviews">
+            <div className="ratingReview">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+            </div>
             <p className='deskOffer'>{i.desk}</p>
-            <Link to={i.btnLink} className='linkOffer'><p>{i.btnText}</p></Link>
+            {/*<Link to={i.btnLink} className='linkOffer'><p>{i.btnText}</p></Link>*/}
+            <div className='userReview'>
+                <img src={i.img} alt='img'/>
+                <div className='usersDataReview'>
+                    <p className='usersNameReview'>{i.userName}</p>
+                    <p className='usersTextReview'>{i.text}</p>
+                </div>
+            </div>
         </div>
     </div>)
 
@@ -31,8 +44,7 @@ const ReviewsCarousel = (props: propsType) => {
         <Carousel className='reviews'
                   showStatus={false}
                   showThumbs={false}
-                  centerSlidePercentage={3}
-
+                  centerSlidePercentage={4}
                   infiniteLoop
                   centerMode
                   useKeyboardArrows
